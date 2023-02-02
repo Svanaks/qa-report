5421b9ce5a2c1a9e82bda22f75c0a0e84eb1c29e
william bloch <bloch.william@gmail.com>
2023-02-02 17:50:08 +0100

    Updates

--- src/assets/index.js
+++
@@ -1,4 +0,0 @@
-export const hgLogo = require('./honore-logo.png')
-export const blueBackground = require('./fond.png')
-export const check = require('./check.png')
-export const bug = require('./bug.png')
